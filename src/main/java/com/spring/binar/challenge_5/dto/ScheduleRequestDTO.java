05320b1a45b84bed57abd3e9cb3d9322f7886098
Abid Bilal <abiddddbilal@gmail.com>
2023-06-02 00:44:16 +0700

    update dto

--- src/main/java/com/spring/binar/challenge_5/dto/ScheduleRequestDTO.java
+++ src/main/java/com/spring/binar/challenge_5/dto/ScheduleRequestDTO.java
@@ -5,6 +5,8 @@ import lombok.Data;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 @Data
 @Builder
 public class ScheduleRequestDTO implements Serializable {
@@ -14,4 +16,14 @@ public class ScheduleRequestDTO implements Serializable {
     private int price;
     private int studioId;
     private int filmId;
+    
+    @JsonCreator
+    public ScheduleRequestDTO(int scheduleId, Long fromDate, Long toDate, int price, int studioId, int filmId){
+        this.scheduleId = scheduleId;
+        this.fromDate = fromDate;
+        this.toDate = toDate;
+        this.price = price;
+        this.studioId = studioId;
+        this.filmId = filmId;
+    }
 }
