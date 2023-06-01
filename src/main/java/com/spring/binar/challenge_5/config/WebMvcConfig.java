1f4b5605a29a8e4a2318ed0d3cbb9e04d3979af3
Abid Bilal <abiddddbilal@gmail.com>
2023-06-02 00:14:02 +0700

    cors update

--- src/main/java/com/spring/binar/challenge_5/config/WebMvcConfig.java
+++ src/main/java/com/spring/binar/challenge_5/config/WebMvcConfig.java
@@ -4,10 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-/**
- * @author Edward P. Legaspi | czetsuya@gmail.com
- * @since
- */
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
@@ -15,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedMethods("*")
-        .allowedOrigins("localhost:3000")
+        .allowedOrigins("http://localhost:3000")
         .allowedHeaders("*")
         .allowCredentials(false)
         .maxAge(-1);
