36997b0ad0d20e633f66284e0dfeed2dc6a43ced
Dwika <dwikariswanda@gmail.com>
2023-06-02 21:18:46 +0700

    Fix Bug costumer, Staff, and schedule

--- src/main/java/com/spring/binar/challenge_5/dto/ScheduleRequestDTO.java
+++ src/main/java/com/spring/binar/challenge_5/dto/ScheduleRequestDTO.java
@@ -3,15 +3,19 @@ package com.spring.binar.challenge_5.dto;
 import com.spring.binar.challenge_5.models.Film;
 import com.spring.binar.challenge_5.models.Schedule;
 import com.spring.binar.challenge_5.models.Studio;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import java.io.Serializable;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ScheduleRequestDTO {
     private long fromDate;
     private long toDate;
@@ -26,7 +30,6 @@ public class ScheduleRequestDTO {
                 .price(price)
                 .studio(studio)
                 .film(film)
-                .scheduleId(0)
                 .build();
     }
 }
--- src/main/java/com/spring/binar/challenge_5/dto/ScheduleResponseDTO.java
+++ src/main/java/com/spring/binar/challenge_5/dto/ScheduleResponseDTO.java
@@ -3,13 +3,17 @@ package com.spring.binar.challenge_5.dto;
 import com.spring.binar.challenge_5.models.Film;
 import com.spring.binar.challenge_5.models.Seat;
 import com.spring.binar.challenge_5.models.Studio;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
 @Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class ScheduleResponseDTO {
     private int scheduleId;
     private long fromDate;
